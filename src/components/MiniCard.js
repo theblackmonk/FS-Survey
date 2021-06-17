9590fee76395dd9fd186ce356bf267663cf88460
theblackmonk <86014667+theblackmonk@users.noreply.github.com>
2021-06-17 09:37:05 -0400

    added comment

--- src/components/MiniCard.js
+++ src/components/MiniCard.js
@@ -1,6 +1,6 @@
 import "./MiniCard.css";
 
-function MiniCard(props) {
+function MiniCard(props) {                                //card for form and title
    const classes = 'minicard ' + props.className;
     return(
         <div className={classes}>{props.children}</div>
